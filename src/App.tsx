351fdd21a488bb68e1667c946cb00c0fb0bbc9c9
shoet <32211937+shoet@users.noreply.github.com>
2024-03-13 20:24:21 +0900

    ページタイトルの挙動を修正する

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ import { SWRConfig } from 'swr'
 import { fetcher } from '@/utils/fetcher'
 import { ApiContext } from '@/types/api'
 import { AuthContextProvider } from '@/contexts/AuthContext'
+import { useTitle } from './hooks/use-title'
+import { APP_PAGE_TITLE } from './constant'
 
 function App() {
+  useTitle(APP_PAGE_TITLE)
   const context: ApiContext = {
     apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
   }
